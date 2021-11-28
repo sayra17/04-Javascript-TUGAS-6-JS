ca1a99dca20d5caf2c88229185b005e7aa114849
sayra17 <agussayra03@gmail.com>
2021-11-28 09:21:38 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+04-Javascript-TUGAS-6-JS
---
+++ tugas6.js
@@ -0,0 +1,10 @@
+function bilangan() {
+    for (var i = 1; i <= 20; i++) {
+        if (i % 2 === 0) {
+            console.log("Genap");
+        } else {
+            console.log(i);
+        }
+    }
+}
+bilangan()
